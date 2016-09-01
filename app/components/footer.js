1ebdbb2d216a9e4d88141b6ae6906234803cbbed
sananiki <157621575@qq.com>
2016-09-01 16:44:24 +0800

    'antd-update'

--- app/components/footer.js
+++ app/components/footer.js
@@ -1,16 +1,94 @@
 import React from 'react';
-import { Tabs, Icon, WhiteSpace } from 'antd-mobile';
-const TabPane = Tabs.TabPane;
+import { TabBar  } from 'antd-mobile';
+
 
 export default class Footer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedTab: 'redTab',
+            notifCount: 0,
+            presses: 0,
+        }
+    }
+
+    renderContent(pageText, num?: number) {
+        return (
+            <div style={{ backgroundColor: 'white', height: '100%' }}>
+                <div style={{ paddingTop: 30, marginLeft: 50 }}>{pageText}</div>
+                <div style={{ margin: 50 }}>{num} re-renders of the {pageText}</div>
+            </div>
+        );
+    }
+
     render() {
         return (
-            <div style={{ position:'absolute',width:'100%',bottom:'0px'}}>                
-                <Tabs type="tabbar" mode="dark" defaultActiveKey="1">
-                    <TabPane tab={<span><Icon type="home" />首页</span>} key="1">选项首页内容</TabPane>
-                    <TabPane tab={<span><Icon type="apple" />选项</span>} key="2">选项好友内容</TabPane>
-                    <TabPane tab={<span><Icon type="setting" />设置</span>} key="3">选项设置的内容</TabPane>
-                </Tabs>
+            <div >
+                <TabBar
+                    unselectedTintColor="#949494"
+                    tintColor="#33A3F4"
+                    barTintColor="white"
+                    >
+                    <TabBar.Item
+                        title="生活"
+                        key="生活"
+                        icon={{ uri: 'https://zos.alipayobjects.com/rmsportal/XLdKiKAwDRXQNhC.png' }}
+                        selectedIcon={{ uri: 'https://zos.alipayobjects.com/rmsportal/iKfBQdGdTMubhXy.png' }}
+                        selected={this.state.selectedTab === 'blueTab'}
+                        onPress={() => {
+                            this.setState({
+                                selectedTab: 'blueTab',
+                            });
+                        } }
+                        >
+                        {this.renderContent('生活 Tab') }
+                    </TabBar.Item>
+                    <TabBar.Item
+                        icon={{ uri: 'https://zos.alipayobjects.com/rmsportal/UNQhIatjpNZHjVf.png' }}
+                        selectedIcon={{ uri: 'https://zos.alipayobjects.com/rmsportal/HLkBvJOKnmOfBPO.png' }}
+                        title="口碑"
+                        key="口碑"
+                        badge={this.state.notifCount > 0 ? this.state.notifCount : undefined}
+                        selected={this.state.selectedTab === 'redTab'}
+                        onPress={() => {
+                            this.setState({
+                                selectedTab: 'redTab',
+                                notifCount: this.state.notifCount + 1,
+                            });
+                        } }
+                        >
+                        {this.renderContent('口碑 Tab', this.state.notifCount) }
+                    </TabBar.Item>
+                    <TabBar.Item
+                        icon={{ uri: 'https://zos.alipayobjects.com/rmsportal/EljxLrJEShWZObW.png' }}
+                        selectedIcon={{ uri: 'https://zos.alipayobjects.com/rmsportal/LWNaMdwAFSmYBFw.png' }}
+                        title="朋友"
+                        key="朋友"
+                        selected={this.state.selectedTab === 'greenTab'}
+                        onPress={() => {
+                            this.setState({
+                                selectedTab: 'greenTab',
+                                presses: this.state.presses + 1,
+                            });
+                        } }
+                        >
+                        {this.renderContent('朋友 Tab', this.state.presses) }
+                    </TabBar.Item>
+                    <TabBar.Item
+                        icon={{ uri: 'https://zos.alipayobjects.com/rmsportal/YWpPVCVOnJoCYhs.png' }}
+                        selectedIcon={{ uri: 'https://zos.alipayobjects.com/rmsportal/WadBBxOIZtDzsgP.png' }}
+                        title="我的"
+                        key="我的"
+                        selected={this.state.selectedTab === 'yellowTab'}
+                        onPress={() => {
+                            this.setState({
+                                selectedTab: 'yellowTab',
+                            });
+                        } }
+                        >
+                        {this.renderContent('我的 Tab', this.state.presses) }
+                    </TabBar.Item>
+                </TabBar>
             </div>
         )
     }
